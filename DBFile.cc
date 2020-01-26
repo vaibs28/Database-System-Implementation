06000fb13d92d84e3c6d4d00b623b93065e536ee
Vaibhav <vsahay28@gmail.com>
2020-01-26 00:21:22 -0500

    load method implementation

--- DBFile.cc
+++ DBFile.cc
@@ -15,35 +15,40 @@
 //constructor initializing the required objects
 DBFile::DBFile() {
 
-    //File instance which will hold the DBFile records
-    //this->file = new File();
-    //Page instance
-    //this->page = new Page();
-    //current pointer to the record
-    //this->current = new Record();
+    this->file = new File();
+    this->readPage = new Page();
+    this->writePage = new Page();
+    this->current = new Record();
 
 }
 
 //destructor
 DBFile::~DBFile() {
-    //delete file;
-    //delete page;
-    //delete current;
+    delete file;
+    delete readPage;
+    delete writePage;
+    delete current;
 }
 
 //loads the DBFile instance from a textfile
 void DBFile::Load(Schema &f_schema, char *loadpath) {
 
-    /*//open the file from the loadpath
-    FILE *tableFile = fopen(loadpath, "r");
+    FILE* tableFile = fopen (loadpath,"r");
     Record temp;
-    //read the record and add to the end of the DBFile
-    while (temp.SuckNextRecord(&f_schema, tableFile) != 0)
+
+    while(temp.SuckNextRecord(&f_schema,tableFile)!=0)
         this->Add(temp);
-    fclose(tableFile);*/
+
+    fclose(tableFile);
 }
 
 void DBFile::Add(Record &addMe) {
+    if(!writePage->Append(&addMe)){
+        file->AddPage(writePage,0);
+        writePage->EmptyItOut();
+        writePage->Append(&addMe);
+    }
+    file->AddPage(writePage,0);
 }
 
 void DBFile::MoveFirst() {
---
+++ cmake-build-debug/CMakeFiles/P1.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P1.dir/Comparison.cc.o"
+  "CMakeFiles/P1.dir/ComparisonEngine.cc.o"
+  "CMakeFiles/P1.dir/DBFile.cc.o"
+  "CMakeFiles/P1.dir/File.cc.o"
+  "CMakeFiles/P1.dir/Record.cc.o"
+  "CMakeFiles/P1.dir/Schema.cc.o"
+  "CMakeFiles/P1.dir/TwoWayList.cc.o"
+  "CMakeFiles/P1.dir/main.cc.o"
+  "P1"
+  "P1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
