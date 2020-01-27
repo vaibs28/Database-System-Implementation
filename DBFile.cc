d19445259d9fb91e64c463454bb41170307a726d
Vaibhav Sahay <vsahay28@gmail.com>
2020-01-27 02:21:37 -0500

    updated with multiple pages changes

--- DBFile.cc
+++ DBFile.cc
@@ -27,20 +27,22 @@ void DBFile::Load(Schema &f_schema, char *loadpath) {
     Record record;
     Page page;
     File file1;
-    int pageOffset = 0;
+
     while (record.SuckNextRecord(&f_schema, fd)) {
         //append the record to page
-        current = &record;
+        //recordCount++;
+        current = &record; //store the address in the current pointer of DBFile
         if (page.Append(&record) == 0) {
-            // cannot fit the new record, so add the new page, clear the records and append the record
-            file1.AddPage(&page, pageOffset);
+            // cannot fit the new record, so add the existing page, clear the page and append the record
+            file1.AddPage(&page, pageOffset++);
             page.EmptyItOut();
             page.Append(&record);
         }
     }
-    file1.AddPage(&page, pageOffset); //add the page into the file;
-    file = &file1;
+    file1.AddPage(&page, pageOffset++);
+    file = &file1;  // store in the file pointer in DBFile
     cout << "Added to file" << endl;
+    //cout<<"number of records="<<recordCount<<endl;
     fclose(fd); //close the opened file
     return;
 }
@@ -49,11 +51,10 @@ void DBFile::Add(Record &addMe) {
     //append the record to the end of the page.
     Page page;
     File file;
-    file.GetPage(&page,0);
-    int pageOffset = 0;
+    file.GetPage(&page,pageOffset);
     if (page.Append(&addMe) == 0) {
         // if the size is greater than the available page size then add a new page and clear it
-        //file.AddPage(page, pageOffset++);
+        file.AddPage(&page, pageOffset++);
         page.EmptyItOut();
         page.Append(&addMe);
     }
